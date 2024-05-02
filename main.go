cb6311feb427c7d725e1acf15c3f14beda0fd6e2
Zach <Zacharydehooge@gmail.com>
2024-05-02 09:42:33 -0400

    Closes #34
    Fixes Issue #34

--- main.go
+++ main.go
@@ -23,7 +23,6 @@ func main() {
 
 	var month, day, year, radar string
 	var timeStart, timeEnd string
-	//var fullDay string
 
 	fmt.Print("Enter Month: ")
 	fmt.Scanln(&month)
@@ -44,31 +43,9 @@ func main() {
 
 	fmt.Print("Enter Radar: ")
 	fmt.Scanln(&radar)
-
-	// TODO: Have user prompted with Full day download
-	// TODO: Handle if else where y timeStart and timeEnd are 000000 and 235959 respectively for y (yes) and continue prompts for user as normal if no or anything other than y (yes)
-	/* 	fmt.Print("Download Full Day?[y/n]")
-	   	fmt.Scanln(&fullDay)
-	   	fullDay1 := strings.ToLower(fullDay)
-	   	if fullDay1 == "y" {
-	   		timeStart = "000000"
-	   		timeEnd = "000000"
-	   	} else {
-	   		fmt.Print("Time Start in Zulu (HHMMSS): ")
-	   		fmt.Scanln(&timeStart)
-	   		// Fixed octal issue by dereferencing the pointer and converting to integer from string that is passed to the CLI
-	   		test := timeStart
-	   		timeStart = strconv.Atoi(timeStart)
-
-	   		fmt.Print("Time End in Zulu (HHMMSS): ")
-	   		fmt.Scanln(&timeEnd)
-	   		test3 := timeEnd
-	   		test4, _ := strconv.Atoi(test3)
-	   	} */
-
 	fmt.Print("Time Start in Zulu (HHMMSS): ")
 	fmt.Scanln(&timeStart)
-	// Fixed octal issue by dereferencing the pointer and converting to integer from string that is passed to the CLI
+
 	test := timeStart
 	test1, _ := strconv.Atoi(test)
 
@@ -79,9 +56,23 @@ func main() {
 
 	for x := test1; x <= test4; x++ {
 
+		var end string
+		year2 := year
+		year1, _ := strconv.Atoi(year2)
+
+		if year1 >= 2009 && year1 <= 2012 {
+			end = "V03"
+		} else if year1 < 2009 {
+			end = ""
+		} else {
+			end = "V06"
+		}
+
 		timeComb := fmt.Sprintf("%06d", x)
 
-		url := fmt.Sprintf("https://noaa-nexrad-level2.s3.amazonaws.com/%s/%s/%s/%s/%s%s%s%s_%s_V06", year, month, day, radar, radar, year, month, day, timeComb)
+		url := fmt.Sprintf("https://noaa-nexrad-level2.s3.amazonaws.com/%s/%s/%s/%s/%s%s%s%s_%s_%s", year, month, day, radar, radar, year, month, day, timeComb, end)
+
+		url2 := fmt.Sprintf("https://noaa-nexrad-level2.s3.amazonaws.com/%s/%s/%s/%s/%s%s%s%s_%s_%s.gz", year, month, day, radar, radar, year, month, day, timeComb, end)
 
 		folderLocation := fmt.Sprintf("%s_%s_%s_%s", day, month, year, radar)
 		if _, err := os.Stat(folderLocation); os.IsNotExist(err) {
@@ -96,10 +87,22 @@ func main() {
 			os.WriteFile(filePath, body, 0644)
 		}
 
+		resp2, err := http.Get(url2)
+		if err == nil && resp2.StatusCode == 200 {
+			fmt.Println("(+) FETCHING .GZ", url2, folderLocation)
+			body, _ := io.ReadAll(resp2.Body)
+			filePath := filepath.Join(folderLocation, fmt.Sprintf("%s_%s_%s_%s_%s", day, month, year, radar, timeComb))
+			os.WriteFile(filePath, body, 0644)
+		}
+
 		// ! Uncomment for Debugging file download
 		/* if err == nil && resp.StatusCode != 200 {
 			fmt.Println("(-) CANNOT FETCH", url, resp.StatusCode)
 		} */
+
+		if err == nil && resp.StatusCode != 200 {
+			fmt.Println("(-) CANNOT FETCH .GZ", url2, resp2.StatusCode)
+		}
 
 		if x == test4 {
 			fmt.Println("Done...")
