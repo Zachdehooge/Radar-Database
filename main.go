888fcc9bad65fb036dd87860e00f23295468666c
Zach <Zacharydehooge@gmail.com>
2024-08-29 11:20:03 -0400

    Closes Issue #5
    Implements Full Day Download

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"path/filepath"
 	"strconv"
 	"time"
-
-	tea "github.com/charmbracelet/bubbletea"
 )
 
 func exeTime(name string) func() {
@@ -19,107 +17,109 @@ func exeTime(name string) func() {
 	}
 }
 
-type model struct {
-	choices  []string         // items on the to-do list
-	cursor   int              // which to-do list item our cursor is pointing at
-	selected map[int]struct{} // which to-do items are selected
-}
-
-func initialModel() model {
-	return model{
-		// Our to-do list is a grocery list
-		choices: []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"},
-
-		// A map which indicates which choices are selected. We're using
-		// the  map like a mathematical set. The keys refer to the indexes
-		// of the `choices` slice, above.
-		selected: make(map[int]struct{}),
+/*
+	 type model struct {
+		choices  []string         // items on the to-do list
+		cursor   int              // which to-do list item our cursor is pointing at
+		selected map[int]struct{} // which to-do items are selected
 	}
-}
 
-func (m model) Init() tea.Cmd {
-	// Just return `nil`, which means "no I/O right now, please."
-	return nil
-}
+	func initialModel() model {
+		return model{
+			// Our to-do list is a grocery list
+			choices: []string{"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"},
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-
-	// Is it a key press?
-	case tea.KeyMsg:
-
-		// Cool, what was the actual key pressed?
-		switch msg.String() {
-
-		// These keys should exit the program.
-		case "ctrl+c", "q":
-			return m, tea.Quit
-
-		// The "up" and "k" keys move the cursor up
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			}
-
-		// The "enter" key and the spacebar (a literal space) toggle
-		// the selected state for the item that the cursor is pointing at.
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
+			// A map which indicates which choices are selected. We're using
+			// the  map like a mathematical set. The keys refer to the indexes
+			// of the `choices` slice, above.
+			selected: make(map[int]struct{}),
 		}
 	}
 
-	// Return the updated model to the Bubble Tea runtime for processing.
-	// Note that we're not returning a command.
-	return m, nil
-}
-
-func (m model) View() string {
-	// The header
-	s := "Please Select A Month\n\n"
-
-	// Iterate over our choices
-	for i, choice := range m.choices {
-
-		// Is the cursor pointing at this choice?
-		cursor := " " // no cursor
-		if m.cursor == i {
-			cursor = ">" // cursor!
-		}
-
-		// Is this choice selected?
-		checked := " " // not selected
-		if _, ok := m.selected[i]; ok {
-			checked = "x" // selected!
-		}
-
-		// Render the row
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	func (m model) Init() tea.Cmd {
+		// Just return `nil`, which means "no I/O right now, please."
+		return nil
 	}
 
-	// The footer
-	s += "\nPress q to quit.\n"
+	func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+		switch msg := msg.(type) {
 
-	// Send the UI for rendering
-	return s
-}
+		// Is it a key press?
+		case tea.KeyMsg:
 
-func main(){
-	p := tea.NewProgram(initialModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
+			// Cool, what was the actual key pressed?
+			switch msg.String() {
+
+			// These keys should exit the program.
+			case "ctrl+c", "q":
+				return m, tea.Quit
+
+			// The "up" and "k" keys move the cursor up
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+
+			// The "down" and "j" keys move the cursor down
+			case "down", "j":
+				if m.cursor < len(m.choices)-1 {
+					m.cursor++
+				}
+
+			// The "enter" key and the spacebar (a literal space) toggle
+			// the selected state for the item that the cursor is pointing at.
+			case "enter", " ":
+				_, ok := m.selected[m.cursor]
+				if ok {
+					delete(m.selected, m.cursor)
+				} else {
+					m.selected[m.cursor] = struct{}{}
+				}
+			}
+		}
+
+		// Return the updated model to the Bubble Tea runtime for processing.
+		// Note that we're not returning a command.
+		return m, nil
 	}
+
+	func (m model) View() string {
+		// The header
+		s := "Please Select A Month\n\n"
+
+		// Iterate over our choices
+		for i, choice := range m.choices {
+
+			// Is the cursor pointing at this choice?
+			cursor := " " // no cursor
+			if m.cursor == i {
+				cursor = ">" // cursor!
+			}
+
+			// Is this choice selected?
+			checked := " " // not selected
+			if _, ok := m.selected[i]; ok {
+				checked = "x" // selected!
+			}
+
+			// Render the row
+			s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		}
+
+		// The footer
+		s += "\nPress q to quit.\n"
+
+		// Send the UI for rendering
+		return s
+	}
+*/
+
+func main() {
+	// p := tea.NewProgram(initialModel())
+	// if _, err := p.Run(); err != nil {
+	// fmt.Printf("Alas, there's been an error: %v", err)
+	// os.Exit(1)
+	// }
 
 	defer exeTime("main")()
 
@@ -149,15 +149,29 @@ func main(){
 	fmt.Print("Folder Location (Paste directory path without the ending \"\\\" (C:\\Test)): ")
 	fmt.Scanln(&filePathFolder)
 
-	fmt.Print("Time Start in Zulu (HHMMSS): ")
+	fmt.Print("Time Start in Zulu (HHMMSS)(Push Enter to Default to 000000): ")
 	fmt.Scanln(&timeStart)
-	test := timeStart
-	test1, _ := strconv.Atoi(test)
 
-	fmt.Print("Time End in Zulu (HHMMSS): ")
+	var test1 int
+	if timeStart != "" {
+		test := timeStart
+		test1, _ = strconv.Atoi(test)
+	} else {
+		test := "000000"
+		test1, _ = strconv.Atoi(test)
+	}
+
+	fmt.Print("Time End in Zulu (HHMMSS)(Push Enter to Default to 235959): ")
 	fmt.Scanln(&timeEnd)
-	test3 := timeEnd
-	test4, _ := strconv.Atoi(test3)
+
+	var test4 int
+	if timeEnd != "" {
+		test3 := timeEnd
+		test4, _ = strconv.Atoi(test3)
+	} else {
+		test3 := "235959"
+		test4, _ = strconv.Atoi(test3)
+	}
 
 	for x := test1; x <= test4; x++ {
 
@@ -203,15 +217,15 @@ func main(){
 			os.WriteFile(filePath, body, 0644)
 		}
 
-		// ! Uncomment for Debugging file download
-		/* if err == nil && resp.StatusCode != 200 {
+		//! Uncomment for Debugging file download
+		if err == nil && resp.StatusCode != 200 {
 			fmt.Println("(-) CANNOT FETCH", url, resp.StatusCode)
 		}
 
-		if err == nil && resp.StatusCode != 200 {
-			fmt.Println("(-) CANNOT FETCH .GZ", url2, resp2.StatusCode)
-		}
-		*/
+		// if err == nil && resp.StatusCode != 200 {
+		// fmt.Println("(-) CANNOT FETCH .GZ", url2, resp2.StatusCode)
+		// }
+
 		if x == test4 {
 			fmt.Println("Done...")
 			return
