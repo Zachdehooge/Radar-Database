c3e109cf4f3a4af29f9fa0d3fe9fc951e5f9cb8a
Zach <47896974+Zachdehooge@users.noreply.github.com>
2025-03-24 11:59:26 -0400

    Added choice for user to supply city and state location instead of just a site code

--- radar-database.go
+++ radar-database.go
@@ -1,18 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/charmbracelet/lipgloss"
+	"github.com/joho/godotenv"
 	"github.com/schollz/progressbar/v3"
 )
+
+type Response struct {
+	Properties struct {
+		RadarStation string `json:"radarStation"`
+	} `json:"properties"`
+}
 
 func exeTime() func() {
 	start := time.Now()
@@ -153,9 +165,12 @@ func downloadTimeRange(radar, year, month, day, filePathFolder string, timeStart
 }
 
 func main() {
+
 	defer exeTime()()
 
 	var month, day, year, radar, filePathFolder string
+	var state string
+	var choice int
 	var timeStart, timeEnd string
 
 	initialText := "Radar Database"
@@ -176,76 +191,246 @@ func main() {
 	fmt.Println(lipgloss.NewStyle().
 		Bold(true).
 		Foreground(lipgloss.Color("#ff00d0")).
-		Render("Enter Month: "))
+		Render("Enter 1 for specific radar site or any key for city state entry): "))
 
-	_, _ = fmt.Scanln(&month)
-	num1, _ := strconv.Atoi(month)
-	if num1 < 10 {
-		month = fmt.Sprintf("%02d", num1)
+	_, _ = fmt.Scanln(&choice)
+
+	if choice == 1 {
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter Month: "))
+
+		_, _ = fmt.Scanln(&month)
+		num1, _ := strconv.Atoi(month)
+		if num1 < 10 {
+			month = fmt.Sprintf("%02d", num1)
+		}
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter Day: "))
+
+		_, _ = fmt.Scanln(&day)
+		num2, _ := strconv.Atoi(day)
+		if num2 < 10 {
+			day = fmt.Sprintf("%02d", num2)
+		}
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter Year: "))
+		_, _ = fmt.Scanln(&year)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter Radar: "))
+		_, _ = fmt.Scanln(&radar)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Folder Location (Paste directory path without the ending \"\\\" (C:\\Test)): "))
+		_, _ = fmt.Scanln(&filePathFolder)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#67ff02")).
+			Render("Time Start in Zulu (HHMMSS): "))
+		_, _ = fmt.Scanln(&timeStart)
+		timeStartInt, _ := strconv.Atoi(timeStart)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#CC0000")).
+			Render("Time End in Zulu (HHMMSS): "))
+		_, _ = fmt.Scanln(&timeEnd)
+		timeEndInt, _ := strconv.Atoi(timeEnd)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#67ff02")).
+			Render("-----------------------------------------\n"))
+
+		// We're making 2 requests per time value (regular and GZ)
+		totalRequests := (timeEndInt - timeStartInt + 1) * 2
+		bar := progressbar.Default(int64(totalRequests))
+
+		// Call the function to download files with goroutines and get the download directory path
+		downloadDir := downloadTimeRange(radar, year, month, day, filePathFolder, timeStartInt, timeEndInt, bar)
+
+		// Check if the download directory is empty and delete if necessary
+		if downloadDir != "" {
+			deleteEmptyFolder(downloadDir)
+		}
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#00ff00")).
+			Render("\nDone...\n"))
 	}
+	if choice != 1 {
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter Month: "))
 
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#ff00d0")).
-		Render("Enter Day: "))
+		_, _ = fmt.Scanln(&month)
+		num1, _ := strconv.Atoi(month)
+		if num1 < 10 {
+			month = fmt.Sprintf("%02d", num1)
+		}
 
-	_, _ = fmt.Scanln(&day)
-	num2, _ := strconv.Atoi(day)
-	if num2 < 10 {
-		day = fmt.Sprintf("%02d", num2)
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter Day: "))
+
+		_, _ = fmt.Scanln(&day)
+		num2, _ := strconv.Atoi(day)
+		if num2 < 10 {
+			day = fmt.Sprintf("%02d", num2)
+		}
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter Year: "))
+		_, _ = fmt.Scanln(&year)
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter City (EX: Atlanta): "))
+		city, _ := reader.ReadString('\n')
+		city = strings.TrimSpace(city)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Enter State (EX: GA): "))
+		_, _ = fmt.Scanln(&state)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#ff00d0")).
+			Render("Folder Location (Paste directory path without the ending \"\\\" (C:\\Test)): "))
+		_, _ = fmt.Scanln(&filePathFolder)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#67ff02")).
+			Render("Time Start in Zulu (HHMMSS): "))
+		_, _ = fmt.Scanln(&timeStart)
+		timeStartInt, _ := strconv.Atoi(timeStart)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#CC0000")).
+			Render("Time End in Zulu (HHMMSS): "))
+		_, _ = fmt.Scanln(&timeEnd)
+		timeEndInt, _ := strconv.Atoi(timeEnd)
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#67ff02")).
+			Render("-----------------------------------------\n"))
+
+		// We're making 2 requests per time value (regular and GZ)
+		totalRequests := (timeEndInt - timeStartInt + 1) * 2
+		bar := progressbar.Default(int64(totalRequests))
+
+		baseURL := "https://geocode.xyz"
+
+		// Create URL parameters
+		params := url.Values{}
+		params.Add("locate", city+", "+state)
+		params.Add("region", "US")
+		params.Add("json", "1")
+
+		// Build the request URL (note: Go's url.Values.Encode() is equivalent to Python's urlencode)
+
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+
+		// Get the API token
+		apiToken := os.Getenv("API_TOKEN")
+		if apiToken == "" {
+			log.Fatal("API_TOKEN is not set")
+		}
+
+		reqURL := fmt.Sprintf("%s/?%s", baseURL, params.Encode()+"&auth="+apiToken)
+
+		// Make the HTTP request
+		resp, err := http.Get(reqURL)
+		if err != nil {
+			fmt.Println("Error:", err)
+			os.Exit(1)
+		}
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		}(resp.Body)
+
+		// Check for HTTP errors
+		if resp.StatusCode != http.StatusOK {
+			fmt.Printf("HTTP error: %s\n", resp.Status)
+			os.Exit(1)
+		}
+
+		// Read and parse the JSON response
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response:", err)
+			os.Exit(1)
+		}
+
+		var geocodeData map[string]interface{}
+		if err := json.Unmarshal(body, &geocodeData); err != nil {
+			fmt.Println("Error parsing JSON:", err)
+			os.Exit(1)
+		}
+
+		citystateURL := fmt.Sprintf("https://api.weather.gov/points/%v,%v", geocodeData["latt"], geocodeData["longt"])
+		fmt.Println(citystateURL)
+
+		resp, err = http.Get(citystateURL)
+		if err != nil {
+			log.Fatalf("Error fetching data: %v", err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			log.Fatalf("API request failed with status: %d", resp.StatusCode)
+		}
+
+		var result Response
+		if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+			log.Fatalf("Error decoding JSON: %v", err)
+		}
+
+		fmt.Println("Radar Station:", result.Properties.RadarStation)
+
+		radar := result.Properties.RadarStation
+
+		downloadDir := downloadTimeRange(radar, year, month, day, filePathFolder, timeStartInt, timeEndInt, bar)
+
+		// Check if the download directory is empty and delete if necessary
+		if downloadDir != "" {
+			deleteEmptyFolder(downloadDir)
+		}
+
+		fmt.Println(lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#00ff00")).
+			Render("\nDone...\n"))
 	}
-
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#ff00d0")).
-		Render("Enter Year: "))
-	_, _ = fmt.Scanln(&year)
-
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#ff00d0")).
-		Render("Enter Radar: "))
-	_, _ = fmt.Scanln(&radar)
-
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#ff00d0")).
-		Render("Folder Location (Paste directory path without the ending \"\\\" (C:\\Test)): "))
-	_, _ = fmt.Scanln(&filePathFolder)
-
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#67ff02")).
-		Render("Time Start in Zulu (HHMMSS): "))
-	_, _ = fmt.Scanln(&timeStart)
-	timeStartInt, _ := strconv.Atoi(timeStart)
-
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#CC0000")).
-		Render("Time End in Zulu (HHMMSS): "))
-	_, _ = fmt.Scanln(&timeEnd)
-	timeEndInt, _ := strconv.Atoi(timeEnd)
-
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#67ff02")).
-		Render("-----------------------------------------\n"))
-
-	// We're making 2 requests per time value (regular and GZ)
-	totalRequests := (timeEndInt - timeStartInt + 1) * 2
-	bar := progressbar.Default(int64(totalRequests))
-
-	// Call the function to download files with goroutines and get the download directory path
-	downloadDir := downloadTimeRange(radar, year, month, day, filePathFolder, timeStartInt, timeEndInt, bar)
-
-	// Check if the download directory is empty and delete if necessary
-	if downloadDir != "" {
-		deleteEmptyFolder(downloadDir)
-	}
-
-	fmt.Println(lipgloss.NewStyle().
-		Bold(true).
-		Foreground(lipgloss.Color("#00ff00")).
-		Render("\nDone...\n"))
 }
